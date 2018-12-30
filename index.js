68c0d1dddd378c76fff46bad583e0f21da51b77e
GermanRodrickson <germanrodrickson@gmail.com>
2018-12-30 19:05:03 +0100

    css hour

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("hola mundo");
+const hour = document.getElementById("hour"); 
+// document.getElementById("p2").style.color = "blue";
+
+setTimeout( () => { 
+  hour.style.transform = "rotateZ(2deg)";
+ }, 1000);
+
+function timeout() {
+  setTimeout(() => {
+    hour.style.transform = "rotateZ(2deg)";
+    timeout();
+  }, 1000);
+}
